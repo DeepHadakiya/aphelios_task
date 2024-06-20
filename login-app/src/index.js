8c73938896abf744889820e936f5607577da458c
DeepHadakiya <138992797+DeepHadakiya@users.noreply.github.com>
2024-06-20 16:22:24 +0530

    Update index.js

--- login-app/src/index.js
+++ login-app/src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const LazyApp = lazy(() => import('./App'))
 root.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={'Loading....'}>
+      <LazyApp />
+    </Suspense>
   </React.StrictMode>
 );
 
